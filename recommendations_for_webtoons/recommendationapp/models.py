826985e54aeec05a9a520cfdc5044a2d9f7420fd
kyj098707 <kyj098707@naver.com>
2023-02-03 20:46:17 +0900

    making db

--- recommendations_for_webtoons/recommendationapp/models.py
+++ recommendations_for_webtoons/recommendationapp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Detail(models.Model):
+    title_id = models.CharField(max_length=15, blank=True, null=True)
+    title = models.CharField(max_length=20, blank=True, null=True)
+    website = models.CharField(max_length=10, blank=True, null=True)
+    story = models.TextField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'detail'
--- recommendations_for_webtoons/recommendations_for_webtoons/settings.py
+++ recommendations_for_webtoons/recommendations_for_webtoons/settings.py
@@ -75,9 +75,13 @@ WSGI_APPLICATION = "recommendations_for_webtoons.wsgi.application"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django_test',
+        'USER': 'root',
+        'PASSWORD': 'dbdnjs12!@',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 

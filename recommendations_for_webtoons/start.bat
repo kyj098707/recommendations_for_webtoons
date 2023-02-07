856a32e40c466ae8f0d585f2ab8780746f667acd
RamdaTech <deokster7@gmail.com>
2023-02-07 22:25:34 +0900

    Clear git cache

--- recommendations_for_webtoons/start.bat
+++
@@ -1,4 +0,0 @@
-C:\Users\{username}\Miniconda3\envs\{env_name}\python.exe manage.py makemigrations
-C:\Users\{username}\Miniconda3\envs\{env_name}\python.exe manage.py migrate
-C:\Users\{username}\Miniconda3\envs\{env_name}\python.exe manage.py runserver
-pause

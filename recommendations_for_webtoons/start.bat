f018e31bbf5643cf85f7d972e53ccd64efd76cb5
RamdaTech <deokster7@gmail.com>
2023-02-07 21:24:34 +0900

    Add batch file and conf
    - db login info
    - auto migrate/runserver batch

---
+++ recommendations_for_webtoons/start.bat
@@ -0,0 +1,4 @@
+C:\Users\{username}\Miniconda3\envs\{env_name}\python.exe manage.py makemigrations
+C:\Users\{username}\Miniconda3\envs\{env_name}\python.exe manage.py migrate
+C:\Users\{username}\Miniconda3\envs\{env_name}\python.exe manage.py runserver
+pause

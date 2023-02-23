4d60e2d19d112146fc2278bac3ff848a480babf0
RamdaTech <deokster7@gmail.com>
2023-02-24 01:24:06 +0900

    Update visual design in service page

---
+++ recommendations_for_webtoons/__static/js/service/main.js
@@ -0,0 +1,63 @@
+// setTimeout(function() {
+//   const modals = document.querySelector('#test_modal');
+//   let modal = bootstrap.Modal.getOrCreateInstance(modals)  
+//   modal.show()
+// }, 250);
+  
+
+function get_artwork_detail(url, value){
+  data = {'keyword' : value,
+          'indicator' : 'get_aw_detail'}
+  res = request_ajax(url, data)
+  if (res != 'error'){
+      $('#modal_detail_artwork').replaceWith(res)
+  }
+
+  const modals = document.querySelector('#modal_detail_artwork');
+  let modal = bootstrap.Modal.getOrCreateInstance(modals)  
+  modal.show()
+}
+    
+
+function send_keyword(url, value){
+    if (value.length >= 2) {
+      data = {'keyword' : value,
+              'indicator' : 'get_startswith'}
+      res = request_ajax(url, data)
+      if (res != 'error') {
+        $('#search_list').replaceWith(res)
+      }
+    }
+}
+
+
+$(document).ready(function(){
+    let sb = document.getElementById('service_banner');
+    let mb = document.getElementById('mask_banner');
+    fadeOutOnScroll(mb, sb);
+})
+
+function fadeOutOnScroll(element, element_o) {
+  if (!element) {
+    return;
+  }
+  let distanceToTop = window.pageYOffset + element.getBoundingClientRect().top;
+  let elementHeight = element.offsetHeight;
+  let scrollTop = document.documentElement.scrollTop;
+  
+  let opacity = 1;
+  if (scrollTop > distanceToTop) {
+    opacity = 1 - (scrollTop) / elementHeight;
+  }
+  element_o.style.opacity = opacity;
+}
+
+function scrollHandler() {
+    let sb = document.getElementById('service_banner');
+    let mb = document.getElementById('mask_banner');
+    fadeOutOnScroll(mb, sb);
+}
+
+window.addEventListener('scroll', scrollHandler);
+
+

6d51ffa5053518880e1eada4565aff5a463ef039
kyj098707 <kyj098707@naver.com>
2023-02-24 23:50:21 +0900

    add : login, logout, signup function

--- recommendations_for_webtoons/recommendationapp/views_account.py
+++ recommendations_for_webtoons/recommendationapp/views_account.py
@@ -10,19 +10,20 @@ def signup_test(request):
     user = request.user
     if user.is_authenticated:
         return HttpResponse("You are already authenticated as " + str(user.email))
+    ### db 겹치면 안되는 부분에 대해서 예외처리 필요
     if request.POST:
         if request.POST['password1'] == request.POST['password2']:
             email = request.POST['email']
             username = request.POST['username']
             password = request.POST['password1']
-            member = authenticate(request,email=email,username=username,password=password)
+            member = Member.objects.create_user(email=email,username=username, password=password)
             nickname, gender, age = request.POST['nickname'], request.POST['gender'], request.POST['age']
-            age = 0 if age == '남' else 1
+            gender = 0 if gender == '남' else 1
             login(request,member)
-            """user = Member.objects.get(email=email)
+            user = Member.objects.get(email=email)
             userprofile = Userprofile.objects.create(member=user,nickname=nickname, gender=gender,age=age)
             userprofile.save()
-            """
+            
             return render(request,'_00_account/login.html')
     return render(request,'_00_account/signup.html')
 
